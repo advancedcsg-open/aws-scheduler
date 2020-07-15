9e65534f2d00210873fdf7d968e6d812eb78b5a8
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-07-15 18:11:00 +0530

    fix(cron): adding model for cron table

--- model.py
+++ model.py
@@ -3,5 +3,6 @@ import os
 import boto3
 
 table_name = f'aws-scheduler-events-v2-{os.environ.get("STAGE")}'
+cron_table_name = f'aws-scheduler-cron-events-v2-{os.environ.get("STAGE")}'
 table = boto3.resource('dynamodb').Table(table_name)
 client = boto3.client('dynamodb')
