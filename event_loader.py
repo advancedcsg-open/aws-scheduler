9b38acdedc67a7e836338d77404bd058ff50d4b1
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-10-21 12:56:45 +0530

    ci(action): updating action to deploy serverless

--- event_loader.py
+++ event_loader.py
@@ -48,7 +48,7 @@ def run():
                 next_date = iter.get_next(datetime)
                 while next_date < current_date:
                     next_date = iter.get_next(datetime)
-                if (next_date - current_date).total_seconds() <= 60 and next_date < datetime.fromisoformat(item['end_date']):
+                if (next_date - current_date).total_seconds() <= 60: # and next_date < datetime.fromisoformat(item['end_date']):
                     item['next_date'] = next_date.isoformat()
                     print(f" item : {item}")
                     items_to_execute.append(item)
